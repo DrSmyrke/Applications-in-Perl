e0222675df6270ac6aa0ebff0b33312ec368cbb6
DrSmyrke <drsmyrke@gmail.com>
2017-02-22 22:27:12 +0300

    Added app to control your's backup

---
+++ backup/backup.pl
@@ -0,0 +1,339 @@
+#!/usr/bin/perl
+# BACKUP, a backup app based on DrSmyrke
+#
+# Any original DrSmyrke code is licensed under the BSD license
+#
+# All code written since the fork of DrSmyrke is licensed under the GPL
+#
+#
+# Copyright (c) 2013 Prokofiev Y. <Smyrke2005@yandex.ru>
+# All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+use Gtk2 '-init';
+use Encode;
+use utf8;
+############## Данные ##################
+$pwd=$0;$pwd=~s#(.*)/.*#$1#;
+if(-l $0){$pwd=readlink $0;$pwd=~s#(.*)/.*#$1#;}
+if(!-f "$pwd/backup.dat"){
+	print "Списка данных для синхронизации не обнаружено!!!\n";
+	exit;
+}
+require "$pwd/config";
+sub getSize{($sz)=@_;if($sz<1024){$st="$sz  байт(а)";}else{if($sz>1024 and $sz<1024000){$sz=substr($sz/1024,0,5);$st="$sz Кб";}else{if($sz>1024000 and  $sz<1024000000){$sz=substr($sz/1048576,0,5);$st="$sz Mб";}else{if($sz>1024000000){$sz=substr($sz/1048576000,0,5);$st="$sz Гб";}}}}return $st;}
+############ Инициализация ###############
+open fs,"<$pwd/backup.dat";@list=<fs>;close fs;chomp @list;
+
+
+
+# создаем главное окно
+$main=Gtk2::Window->new('toplevel');
+$main->set_title("Backup v1.0");
+$main->set_default_size("650","480");
+$main->set_position("GTK_WIN_POS_CENTER_ALWAYS");
+$main->set_resizable(1);
+# Обрабочики сигналов окна
+$main->signal_connect("destroy"=>sub{Gtk2->main_quit;});
+# Построение окна
+$vbox=Gtk2::VBox->new;
+	$pbox=Gtk2::HBox->new;
+		$hfreebar=Gtk2::ProgressBar->new;
+	$pbox->pack_start($hfreebar,1,1,0);
+		$bfreebar=Gtk2::ProgressBar->new;
+	$pbox->pack_start($bfreebar,1,1,0);
+$vbox->pack_start($pbox,0,1,0);
+$tmp=Gtk2::ScrolledWindow->new;
+$tmp->set_policy("automatic","automatic");
+	$windbackup=Gtk2::Viewport->new;
+		$tablekont=Gtk2::VBox->new;
+			$table=Gtk2::Table->new(1,7,FALSE);
+				$table->attach_defaults(Gtk2::Label->new("HOME"),0,4,0,1);
+				$table->attach_defaults(Gtk2::VSeparator->new,4,5,0,1);
+				$table->attach_defaults(Gtk2::Label->new("BACKUP"),5,9,0,1);
+		$tablekont->pack_start($table,0,0,0);
+		$i=0;
+		backupBuild();
+	$windbackup->add($tablekont);
+$tmp->add($windbackup);
+$vbox->add($tmp);
+$vbox->pack_start(Gtk2::HSeparator->new,0,1,0);
+	$hbox=Gtk2::HBox->new;
+		$butbackupupd=Gtk2::Button->new;
+		$butbackupupd->set_image(Gtk2::Image->new_from_stock("gtk-refresh",button));
+		$butbackupupd->set_tooltip_text("Обновить данные");
+		$butbackupupd->signal_connect("clicked"=>sub{backupUPD();});
+	$hbox->pack_start($butbackupupd,0,0,0);
+		$butbackupgohome=Gtk2::Button->new;
+		$butbackupgohome->set_image(Gtk2::Image->new_from_stock("gtk-go-back",button));
+		$butbackupgohome->set_tooltip_text("Извлечь из бэкапа");
+		$butbackupgohome->signal_connect("clicked"=>sub{backupEx();});
+	$hbox->pack_start($butbackupgohome,0,0,0);
+		$butbackupgobackup=Gtk2::Button->new;
+		$butbackupgobackup->set_image(Gtk2::Image->new_from_stock("gtk-go-forward",button));
+		$butbackupgobackup->set_tooltip_text("В бэкап");
+		$butbackupgobackup->signal_connect("clicked"=>sub{backupBack();});
+	$hbox->pack_start($butbackupgobackup,0,0,0);
+$vbox->pack_start($hbox,0,0,0);
+	$backupstat=Gtk2::Label->new;
+	$backupstat->set_alignment(0,0.5);
+$vbox->pack_start($backupstat,0,1,0);
+$main->add($vbox);
+$main->show_all;
+# инициализация
+backupUPD();
+Gtk2->main;
+
+sub getElem{
+	($conttype,$nam)=@_;
+	$ch=$cb=-1;
+
+	if($conttype eq "DIR" or $conttype eq "ARJ"){
+		if(-d "$home/$nam"){
+			$c=`find $home/$nam/ -type l | wc -l`;chomp $c;$ch+=$c;
+			$c=`find $home/$nam/ -type f | wc -l`;chomp $c;$ch+=$c;
+			$ch++;
+		}
+		if(-d "$backup/$nam"){
+			$c=`find $backup/$nam/ -type l | wc -l`;chomp $c;$cb+=$c;
+			$c=`find $backup/$nam/ -type f | wc -l`;chomp $c;$cb+=$c;
+			$cb++;
+		}
+		if(-f "$backup/$nam.7z"){
+			@cmd=`7z l $backup/$nam.7z`;
+			($cmd,$cmd,$cb)=split(" ",pop @cmd);
+		}
+	}
+	if($conttype eq "FILE"){
+		if(-f "$home/$nam"){$ch=1;}
+		if(-f "$backup/$nam"){$cb=1;}
+	}
+	$clh=$clb="valblack";
+	$t="";
+	if($ch>=0){
+		if($cb>=0){
+			$t=($cb-$ch);
+			if($cb<$ch){$clh="valgreen";$clb="valorange";}
+			if($cb>$ch){$t="+$t";$clh="valorange";$clb="valgreen";}
+		}
+	}
+	if($cb<0){$cb="N/A";$clb="valfiol";}
+	if($ch<0){$ch="N/A";$clh="valfiol";}
+	return (eval("\$$clh"),$ch,eval("\$$clb"),$cb,$t);
+}
+sub getSizeStr{
+	($conttype,$nam)=@_;
+	$ch=-1;$cb=-1;
+	if($conttype eq "DIR" or $conttype eq "ARJ"){
+		if(-d "$home/$nam"){
+			@cmd=`du -b -c $home/$nam`;($ch)=split(" ",pop @cmd);
+			($device,$inode,$m,$nlink,$uid,$gid,$rdev,$size,$atime,$mtime,$ctime,$blksize,$blocks)=stat("$home/$nam");$mtimeh=$mtime;
+		}
+		if(-d "$backup/$nam"){
+			@cmd=`du -b -c $backup/$nam`;($cb)=split(" ",pop @cmd);
+			($device,$inode,$m,$nlink,$uid,$gid,$rdev,$size,$atime,$mtime,$ctime,$blksize,$blocks)=stat("$backup/$nam");$mtimeb=$mtime;
+		}
+		if(-f "$backup/$nam.7z"){
+			($device,$inode,$m,$nlink,$uid,$gid,$rdev,$size,$atime,$mtime,$ctime,$blksize,$blocks)=stat("$backup/$nam.7z");$cb=$size;$mtimeb=$mtime;
+		}
+	}
+	if($conttype eq "FILE"){
+		if(-f "$home/$nam"){
+			($device,$inode,$m,$nlink,$uid,$gid,$rdev,$size,$atime,$mtime,$ctime,$blksize,$blocks)=stat("$home/$nam");$ch=$size;$mtimeh=$mtime;
+		}
+		if(-f "$backup/$nam"){
+			($device,$inode,$m,$nlink,$uid,$gid,$rdev,$size,$atime,$mtime,$ctime,$blksize,$blocks)=stat("$backup/$nam");$cb=$size;$mtimeb=$mtime;
+		}
+	}
+	$clh=$clb=$clh2=$clb2="valblack";
+	$t=$t2="";
+	if($ch>=0){
+		if($cb>=0){
+			$t=($cb-$ch);
+			if($cb<$ch){$clh="valgreen";$clb="valorange";}
+			if($cb>$ch){$t="+$t";$clh="valorange";$clb="valgreen";}
+		}
+	}
+	if($mtimeh>=0){
+		if($mtimeb>=0){
+			$t2=($mtimeb-$mtimeh);
+			if($mtimeb<$mtimeh){$clh2="valgreen";$clb2="valorange";}
+			if($mtimeb>$mtimeh){$t2="+$t2";$clh2="valorange";$clb2="valgreen";}
+		}
+	}
+	if($cb<0){$cb="N/A";$clb="valfiol";}
+	if($ch<0){$ch="N/A";$clh="valfiol";}
+	if($ch!="N/A"){$ch=getSize($ch);}
+	if($cb!="N/A"){$cb=getSize($cb);}
+	@time=localtime($mtimeh);
+	$year=1900+$time[5];$month=1+$time[4];$day=$time[3];
+	$day=($day<10)?"0$day":$day;
+	$month=($month<10)?"0$month":$month;
+	$mtimeh="$day.$month.$year";
+	@time=localtime($mtimeb);
+	$year=1900+$time[5];$month=1+$time[4];$day=$time[3];
+	$day=($day<10)?"0$day":$day;
+	$month=($month<10)?"0$month":$month;
+	$mtimeb="$day.$month.$year";
+	if($mtimeb=="01.01.1970"){$mtimeb="N/A";$clb2="valfiol";if($mtimeh!="N/A"){$clh2="valblack";}}
+	if($mtimeh=="01.01.1970"){$mtimeh="N/A";$clh2="valfiol";if($mtimeb!="N/A"){$clb2="valblack";}}
+	return (eval("\$$clh"),$ch,eval("\$$clb"),$cb,eval("\$$clh2"),$mtimeh,eval("\$$clb2"),$mtimeb);
+}
+sub backupBackup{
+	$backupstat->set_text(">: Backup ... ");
+	$fd=$file_dialog->get_filename();
+	system "rm -rf $fd/* & cp -r . $fd";
+	$backupstat->set_text($backupstat->get_text." OK");
+	system 'play.Зефште "$HOME/bin/data/sounds/Задание_выполнено.wav" | notify-send -i gnome-terminal "Резервное копирование завершено !!!"';
+}
+sub backupBuild{
+	foreach $str(@list){
+		($conttype,$name)=split("	",$str);
+		$name=decode("utf8",$name);
+		$but=Gtk2::Button->new;
+		eval "\$but->signal_connect(\"clicked\"=>sub{\$select=\"$name\";\$type=\"$conttype\";if(\$mode){\$butbackupgohome->show;\$butbackupgobackup->show;}\$backupstat->set_text(\">: Selected: [\$type->\$select]   \");});";
+		$but->set_relief("none");
+		$tablekont->pack_start($but,0,1,0);
+		$butvbox=Gtk2::VBox->new;
+			$hbox=Gtk2::HBox->new;
+			$hbox->pack_start(Gtk2::Label->new($name),0,0,0);
+			$hbox->add(Gtk2::HSeparator->new);
+		$butvbox->pack_start($hbox,0,1,0);
+			$table=Gtk2::Table->new(1,7,FALSE);
+			$table->set_homogeneous(1);
+			eval "\$labelhe$i=Gtk2::Label->new(\"$i\");";
+			eval "\$labelbe$i=Gtk2::Label->new(\"$i\");";
+			eval "\$labelhe$i->set_alignment(0,0.5);";
+			eval "\$labelbe$i->set_alignment(0,0.5);";
+			$table->attach_defaults(eval "\$labelhe$i",0,1,0,1);
+			$table->attach_defaults(eval "\$labelbe$i",4,5,0,1);
+			eval "\$labelhs$i=Gtk2::Label->new(\"$i\");";
+			eval "\$labelbs$i=Gtk2::Label->new(\"$i\");";
+			eval "\$labelhs$i->set_alignment(0,0.5);";
+			eval "\$labelbs$i->set_alignment(0,0.5);";
+			$table->attach_defaults(eval "\$labelhs$i",1,2,0,1);
+			$table->attach_defaults(eval "\$labelbs$i",5,6,0,1);
+			eval "\$labelhd$i=Gtk2::Label->new(\"$i\");";
+			eval "\$labelbd$i=Gtk2::Label->new(\"$i\");";
+			eval "\$labelhd$i->set_alignment(0,0.5);";
+			eval "\$labelbd$i->set_alignment(0,0.5);";
+			$table->attach_defaults(eval "\$labelhd$i",2,3,0,1);
+			$table->attach_defaults(eval "\$labelbd$i",6,7,0,1);
+			$table->attach_defaults(Gtk2::VSeparator->new,3,4,0,1);
+		$butvbox->pack_start($table,0,1,0);
+	$but->add($butvbox);
+	$i++;
+	}
+}
+sub backupEx{
+	if($type eq "ARJ"){
+		if(-d "$home/$select" or -f "$home/$select"){system "rm -r $home/$select";}
+		cmd("7z x $backup/$select.7z -o$home");
+	}
+	if($type eq "FILE"){
+		if(-f "$home/$select"){system "rm $home/$select";}
+		cmd("cp -v $backup/$select $home");
+	}
+	if($type eq "DIR"){
+		if(-d "$home/$select"){system "rm -r $home/$select/*";}
+		cmd("cp -r -v $backup/$select $home");
+	}
+	backupUPD();
+}
+sub backupBack{
+	if($type eq "ARJ"){
+		if(-f "$backup/$select.7z"){unlink "$backup/$select.7z";}
+		cmd("7z a -mx=$backuppack $backup/$select.7z $home/$select");
+	}
+	if($type eq "FILE"){
+		if(-f "$backup/$select"){unlink "$backup/$select";}
+		cmd("cp -v $home/$select $backup");
+	}
+	if($type eq "DIR"){
+		if(-d "$backup/$select"){system "rm -r $backup/$select/*";}
+		cmd("cp -r -v $home/$select $backup");
+	}
+	backupUPD();
+}
+sub backupUPD{
+	$backup="N/A";
+	$mode=0;
+	if(!-d $backupdir){cmd($backupDirOpenCommand);print "123\n";}
+	if(-d $backupdir){$backup=$backupdir;$mode=1;}
+	$butbackupgohome->hide;
+	$butbackupgobackup->hide;
+	getFreeSpaceHDD($home,$hfreebar);
+	if($mode){
+		getFreeSpaceHDD($backup,$bfreebar);
+	}else{
+		$bfreebar->set_fraction(0);
+		$bfreebar->set_text("N/A");
+	}
+	$i=0;
+	foreach $str(@list){
+		($conttype,$name)=split("	",$str);
+		$name=decode("utf8",$name);
+		$backupstat->set_text(">: Searching:  [$conttype->$name] ... ");Gtk2->main_iteration while Gtk2->events_pending;
+		($colh,$filesh,$colb,$filesb,$rb)=getElem($conttype,$name);
+		eval "\$labelhe$i->set_markup(\"<span color='$colh'>$filesh</span>\");";
+		eval "\$labelbe$i->set_markup(\"<span color='$colb'>$filesb</span>\");";
+		eval "\$labelhe$i->set_tooltip_text(\"$rb\");";
+		eval "\$labelbe$i->set_tooltip_text(\"$rb\");";
+		($colh,$sizeh,$colb,$sizeb,$colh2,$mtimeh,$colb2,$mtimeb)=getSizeStr($conttype,$name);
+		eval "\$labelhs$i->set_markup(\"<span color='$colh'>$sizeh</span>\");";
+		eval "\$labelbs$i->set_markup(\"<span color='$colb'>$sizeb</span>\");";
+		eval "\$labelhs$i->set_tooltip_text(\"$rb\");";
+		eval "\$labelbs$i->set_tooltip_text(\"$rb\");";
+
+		eval "\$labelhd$i->set_markup(\"<span color='$colh2'>$mtimeh</span>\");";
+		eval "\$labelbd$i->set_markup(\"<span color='$colb2'>$mtimeb</span>\");";
+		eval "\$labelhd$i->set_tooltip_text(\"$rb\");";
+		eval "\$labelbd$i->set_tooltip_text(\"$rb\");";
+#		print "[$conttype,$name]\n";
+		$i++;
+	}
+	$backupstat->set_text($backupstat->get_text."OK");Gtk2->main_iteration while Gtk2->events_pending;
+}
+sub getFreeSpaceHDD{
+	($dir,$bar)=@_;
+	@tmp=`df -x devtmpfs -x tmpfs -B 1 2>/dev/null`;
+	shift @tmp;
+	$find=$prz=0;
+	foreach $str(@tmp){
+		($dev,$tot,$isp,$dost,$mpoint,$mpoint)=split(" ",$str);
+		@tmp2=split("/",$dir);
+		$count=@tmp2;
+		while($count>0){
+			$str=join "/",@tmp2;
+			if($str eq ""){$str="/";}
+			if($str eq $mpoint){
+				#$prz2=int(($dost)/($tot/100));
+				$prz=int($isp/($tot/100));
+				$prz2=100-$prz;
+				$dost=getSize($dost);
+				$tot=getSize($tot);
+				$prz/=100;
+				$bar->set_fraction($prz);
+				$bar->set_text("[$dir] -> Free: $dost / $tot [$prz2%]");
+				$find=1;
+				break;
+			}
+			pop @tmp2;
+			$count=@tmp2;
+		}
+		if($find eq 1){break;}
+	}
+}
+sub cmd{$cmd=shift @_;system "$term '$cmd'";}
